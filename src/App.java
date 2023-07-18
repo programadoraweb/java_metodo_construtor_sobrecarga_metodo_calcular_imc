0e79eae681709dfc4013f5122e629b378a19361e
pw <programadoraweb3.0@gmail.com>
2023-07-18 18:30:22 -0300

    upload files

---
+++ src/App.java
@@ -0,0 +1,18 @@
+﻿public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // Nota: PRIMEIRO voc tem que construir o arquivo com a classe e so depois fazer a declaracao da linha abaixo, senao da bug.
+        Pessoa brasileiro = new Pessoa(null);
+        brasileiro.nome = "Fernanda"; 
+        brasileiro.PrintPessoa();
+
+        Pessoa alemao = new Pessoa(null);
+        alemao.nome = "Xuxa"; // da o nome
+        alemao.PrintPessoa(); // imprime o nome e a cor
+        alemao.ResultadoCalcularIMC(80, 1.70);
+
+        Pessoa grego = new Pessoa(null);
+        grego.ResultadoCalcularIMC(40, 1.50);
+
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,39 @@
+﻿public class Pessoa {
+
+// atributos
+public String nome; 
+public String cor; 
+public int idade;
+protected String sexo;
+double peso;
+double altura;
+
+ //Metodos:
+
+ // Metodo Constructor: 
+ // como eh um metodo que recebe um parametro eh uma "Sobrecarga de Metodo"
+ public Pessoa (String nome) {
+        this.nome = nome; 
+        cor = " amarelo ";
+ }
+
+ // metodo para imprimir informacoes da pessoa
+ public void PrintPessoa(){
+       System.out.println("seu nome eh: " + nome + ", sua cor eh: " + cor);}
+
+public void ResultadoCalcularIMC(double pes, double alt){
+    double imc; 
+    peso=pes;
+//this.peso = pes;
+//this.altura = alt;
+    altura= alt; 
+    imc = peso/(altura*altura);
+     System.out.println("seu imc eh: " + imc);
+}
+
+
+/* public void ResultadoCalcularIMC(resultado){
+    System.out.println(imc);
+
+} */
+}
